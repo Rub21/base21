c08ac9c106c1e26c1f9f9ecbe126b0077971af95
Rub21 <ruben@mapbox.com>
2015-09-25 15:15:11 -0500

    base

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var turf = require('turf');
+var button = document.querySelector('#button');
+button.addEventListener('click', function(e) {
+  e.preventDefault();
+var geom= document.querySelector('#geom').textContent;
+aler('new');
+}, false);
